433aaf1c5f2cbaeaaa386ab7db22c43ac67a0a8d
Fumikazu Fujiwara <github@ze.gs>
2022-08-02 06:51:17 +0900

    complete 02

--- vending_machine.py
+++ vending_machine.py
@@ -3,4 +3,15 @@
 
 class VendingMachine:
     def __init__(self):
-        return
+        self.total = 0
+
+    def input(self,coin):
+        if coin in [10,50,100,500,1000]:
+            self.total += coin
+            return
+        raise RuntimeError('coin should be 10,50,100,500 or 1000')
+
+    def refund(self):
+        prev_total = self.total
+        self.total = 0
+        return prev_total
--- vending_machine_test.py
+++ vending_machine_test.py
@@ -8,3 +8,28 @@ from vending_machine import VendingMachine
 def test_instance():
     vm = VendingMachine()
     assert "VendingMachine" == type(vm).__name__
+    assert 0 == vm.total
+
+def test_input():
+    vm = VendingMachine()
+    vm.input(10)
+    assert 10 == vm.total
+
+def test_refund():
+    vm = VendingMachine()
+    vm.input(10)
+    vm.input(50)
+    vm.input(100)
+    vm.input(500)
+    assert 660 == vm.refund()
+    assert 0 == vm.total
+
+def test_input_exception():
+    vm = VendingMachine()
+    for c in [-1,"a",0,49,2000]:
+        try:
+            vm.input(c)
+        except RuntimeError:
+            continue
+        else:
+            fail("not throwing for " + str(c))
